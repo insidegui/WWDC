67d7fb52c2f216e81d25e3a96deebaba95b0bbc4
Guilherme Rambo <insidegui@gmail.com>
2015-04-29 15:22:34 -0300

    Included Crashlytics

---
+++ WWDC/GRCrashlyticsHelper.h
@@ -0,0 +1,15 @@
+//
+//  GRCrashlyticsHelper.h
+//  WWDC
+//
+//  Created by Guilherme Rambo on 29/04/15.
+//  Copyright (c) 2015 Guilherme Rambo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GRCrashlyticsHelper : NSObject
+
++ (void)install;
+
+@end
