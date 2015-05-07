8e241cd79b0aa0e2481558e1a4a131276195fd00
Guilherme Rambo <insidegui@gmail.com>
2015-05-07 15:04:46 -0300

    Added the possibility to disable automatic updates by passing the argument disable-updates

--- Updater/Updater/UDUpdater.m
+++ Updater/Updater/UDUpdater.m
@@ -42,6 +42,13 @@
 
 - (void)checkForUpdatesWithCompletionHandler:(void (^)(UDRelease *latestRelease))callback
 {
+    if ([[NSProcessInfo processInfo].arguments containsObject:@"disable-updates"]) {
+        #ifdef DEBUG
+        NSLog(@"Updater: disable-updates argument is present, automatic updates disabled");
+        #endif
+        return;
+    }
+    
     [[self.session dataTaskWithURL:self.releasesURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (error) {
             #ifdef DEBUG
