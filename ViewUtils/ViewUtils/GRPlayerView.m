2dc20cc569005732ce3531bfe10e2fff91a7e9ba
Guilherme Rambo <insidegui@gmail.com>
2015-09-30 21:25:31 -0300

    Added speed control (Implements #85)

--- ViewUtils/ViewUtils/GRPlayerView.m
+++ ViewUtils/ViewUtils/GRPlayerView.m
@@ -9,6 +9,8 @@
 #import "GRPlayerView.h"
 #import "GRPlayerWindow.h"
 
+@import AVFoundation;
+
 @interface AVPlayerView ()
 
 - (BOOL)_mouseInNoHideArea;
@@ -21,11 +23,18 @@
 @property (readonly) GRPlayerWindow *playerWindow;
 @property (strong) NSTimer *titlebarTimer;
 
+@property (readonly) NSView *controlsContainerView;
+@property (strong) NSButton *speedButton;
+
 @end
 
 #define kHideTitlebarTimerInterval 3.0f
 
 @implementation GRPlayerView
+{
+    float _storedRate;
+    NSView *_controlsContainerView;
+}
 
 - (void)setupHideTitlebarTimer
 {
@@ -37,7 +46,10 @@
 {
     [super viewDidMoveToWindow];
     
+    _storedRate = 1.0;
+    
     [self setupHideTitlebarTimer];
+    [self setupExtraControls];
 }
 
 - (void)hideTitlebar
@@ -73,6 +85,84 @@
             [self.window.windowController toggleFullScreen:self];
         }
     }
+}
+
+- (NSView *)controlsContainerView
+{
+    if (!_controlsContainerView) {
+        for (NSView *subview in self.subviews) {
+            for (NSView *view in subview.subviews) {
+                if ([view isKindOfClass:NSClassFromString(@"AVMovableView")]) {
+                    _controlsContainerView = view;
+                    break;
+                }
+            }
+        }
+    }
+    
+    return _controlsContainerView;
+}
+
+- (void)setSpeedButtonTitleForRate:(float)rate
+{
+    if (rate <= 0) return;
+
+    if (rate - (int)rate != 0) {
+        [self setSpeedButtonTitle:[NSString stringWithFormat:@"%.1fx", rate]];
+    } else {
+        [self setSpeedButtonTitle:[NSString stringWithFormat:@"%.0fx", rate]];
+    }
+    
+}
+
+- (void)setSpeedButtonTitle:(NSString *)title
+{
+    NSMutableParagraphStyle *pStyle = [[NSMutableParagraphStyle alloc] init];
+    pStyle.alignment = NSCenterTextAlignment;
+    NSDictionary *attrs = @{
+                            NSForegroundColorAttributeName: [NSColor labelColor],
+                            NSParagraphStyleAttributeName: pStyle};
+    self.speedButton.attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:attrs];
+}
+
+- (void)setupExtraControls
+{
+    self.speedButton = [[NSButton alloc] initWithFrame:NSMakeRect(360.0, 31.0, 32.0, 22.0)];
+    NSButtonCell *buttonCell = self.speedButton.cell;
+    buttonCell.highlightsBy = 12;
+    self.speedButton.bordered = NO;
+    self.speedButton.appearance = [NSAppearance appearanceNamed:NSAppearanceNameVibrantDark];
+    self.speedButton.bezelStyle = NSRoundedBezelStyle;
+    self.speedButton.target = self;
+    self.speedButton.action = @selector(speedButtonAction:);
+    [self setSpeedButtonTitle:@"1x"];
+    [self.controlsContainerView addSubview:self.speedButton];
+}
+
+- (void)speedButtonAction:(id)sender
+{
+    if (self.player.rate == 0) return;
+    
+    if (self.player.rate == 1.0) {
+        self.player.rate = 1.5;
+    } else if (self.player.rate == 1.5) {
+        self.player.rate = 2.0;
+    } else if (self.player.rate == 2.0) {
+        self.player.rate = 0.5;
+    } else if (self.player.rate == 0.5) {
+        self.player.rate = 1.0;
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"playerController.playing"]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self setSpeedButtonTitleForRate:self.player.rate];
+        });
+    }
+    
+    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
 }
 
 - (GRPlayerWindow *)playerWindow
