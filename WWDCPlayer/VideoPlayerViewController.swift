46049dc400d54a7e5c8f007afa34f99f0be6e73b
Guilherme Rambo <insidegui@gmail.com>
2017-04-23 18:52:17 -0300

    Integrated PiP

--- WWDCPlayer/VideoPlayerViewController.swift
+++ WWDCPlayer/VideoPlayerViewController.swift
@@ -9,6 +9,7 @@
 import Cocoa
 import AVFoundation
 import AVKit
+import PIPContainer
 
 public struct VideoPlayerViewControllerMetadata {
     public let title: String?
@@ -24,7 +25,10 @@ public struct VideoPlayerViewControllerMetadata {
     }
 }
 
-open class VideoPlayerViewController: NSViewController {
+final class WPLMainView: NSView { }
+final class WPLVideoView: NSView { }
+
+open class VideoPlayerViewController: PIPContainerViewController {
 
     open let metadata: VideoPlayerViewControllerMetadata
     open let player: AVPlayer
@@ -42,13 +46,12 @@ open class VideoPlayerViewController: NSViewController {
         fatalError("VideoPlayerViewController can't be initialized with a coder")
     }
     
-    fileprivate lazy var playerView: AVPlayerView = {
-        let p = AVPlayerView(frame: NSZeroRect)
+    fileprivate lazy var playerContainer: PlayerContainerViewController = {
+        let c = PlayerContainerViewController(player: self.player)
         
-        p.translatesAutoresizingMaskIntoConstraints = false
-        p.controlsStyle = .floating
+        c.view.frame = self.view.bounds
         
-        return p
+        return c
     }()
     
     fileprivate lazy var progressIndicator: NSProgressIndicator = {
@@ -69,13 +72,18 @@ open class VideoPlayerViewController: NSViewController {
     }()
     
     open override func loadView() {
-        view = NSView(frame: NSZeroRect)
+        // ¯\_(ツ)_/¯
+        pipWillOpen = { }
+        
+        view = WPLMainView(frame: NSZeroRect)
         view.wantsLayer = true
         view.layer?.backgroundColor = NSColor.black.cgColor
         
-        view.addSubview(playerView)
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-(0)-[playerView]-(0)-|", options: [], metrics: nil, views: ["playerView": playerView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[playerView]-(0)-|", options: [], metrics: nil, views: ["playerView": playerView]))
+        addChildViewController(playerContainer)
+        
+        playerContainer.view.autoresizingMask = [.viewWidthSizable, .viewHeightSizable]
+        playerContainer.view.frame = view.bounds
+        view.addSubview(playerContainer.view)
         
         view.addSubview(progressIndicator)
         view.addConstraints([
@@ -89,33 +97,16 @@ open class VideoPlayerViewController: NSViewController {
     override open func viewDidLoad() {
         super.viewDidLoad()
         
-        registerPlayerObservationForKeyPath("status") { [weak self] in
-            guard let weakSelf = self else { return }
-            
-            switch weakSelf.player.status {
-            case .readyToPlay, .failed:
-                weakSelf.progressIndicator.stopAnimation(nil)
-                weakSelf.progressIndicator.isHidden = true
-            default: break
-            }
-        }
-        
-        registerPlayerObservationForKeyPath("currentItem.presentationSize") { [weak self] in
-            guard let weakSelf = self else { return }
-            
-            guard let size = weakSelf.player.currentItem?.presentationSize, size != NSZeroSize else { return }
-            
-            (weakSelf.view.window as? VideoPlayerWindow)?.aspectRatio = size
-        }
+        player.addObserver(self, forKeyPath: #keyPath(AVPlayer.status), options: [.initial, .new], context: nil)
+        player.addObserver(self, forKeyPath: #keyPath(AVPlayer.currentItem.presentationSize), options: [.initial, .new], context: nil)
         
         progressIndicator.startAnimation(nil)
         
         title = metadata.title
-        playerView.player = player
         
         let doubleClickGesture = NSClickGestureRecognizer(target: self, action: #selector(doubleClickedPlayerView))
         doubleClickGesture.numberOfClicksRequired = 2
-        playerView.addGestureRecognizer(doubleClickGesture)
+        playerContainer.view.addGestureRecognizer(doubleClickGesture)
         
         player.play()
     }
@@ -130,20 +121,30 @@ open class VideoPlayerViewController: NSViewController {
     
     // MARK: - Player Observation
     
-    fileprivate var playerObservations = Dictionary<String, () -> Void>()
-    
-    fileprivate func registerPlayerObservationForKeyPath(_ keyPath: String, callback: @escaping () -> ()) {
-        playerObservations[keyPath] = callback
-        player.addObserver(self, forKeyPath: keyPath, options: [.initial, .new], context: nil)
-    }
-    
     open override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         guard let keyPath = keyPath else { return }
         
-        if let callback = playerObservations[keyPath] {
-            DispatchQueue.main.async(execute: callback)
+        if keyPath == #keyPath(AVPlayer.currentItem.presentationSize) {
+            DispatchQueue.main.async(execute: playerItemPresentationSizeDidChange)
+        } else if keyPath == #keyPath(AVPlayer.status) {
+            DispatchQueue.main.async(execute: playerStatusDidChange)
         } else {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func playerItemPresentationSizeDidChange() {
+        guard let size = player.currentItem?.presentationSize, size != NSZeroSize else { return }
+        
+        (view.window as? VideoPlayerWindow)?.aspectRatio = size
+    }
+    
+    private func playerStatusDidChange() {
+        switch player.status {
+        case .readyToPlay, .failed:
+            progressIndicator.stopAnimation(nil)
+            progressIndicator.isHidden = true
+        default: break
         }
     }
     
@@ -169,7 +170,41 @@ open class VideoPlayerViewController: NSViewController {
             Swift.print("VideoPlayerViewController is gone")
         #endif
         
-        playerObservations.forEach({ self.player.removeObserver(self, forKeyPath: $0.0) })
+        player.removeObserver(self, forKeyPath: #keyPath(AVPlayer.currentItem.presentationSize))
+        player.removeObserver(self, forKeyPath: #keyPath(AVPlayer.status))
+    }
+    
+}
+
+private final class PlayerContainerViewController: NSViewController {
+    
+    weak var player: AVPlayer?
+    
+    init(player: AVPlayer) {
+        self.player = player
+        
+        super.init(nibName: nil, bundle: nil)!
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = WPLVideoView(frame: .zero)
+        view.wantsLayer = true
+        view.layer = CALayer()
+        view.layer?.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+        view.layer?.backgroundColor = NSColor.black.cgColor
+        
+        guard let player = player else { return }
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.backgroundColor = NSColor.black.cgColor
+        playerLayer.frame = view.bounds
+        playerLayer.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspect
+        view.layer?.addSublayer(playerLayer)
     }
     
 }
