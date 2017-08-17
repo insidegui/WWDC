df29b8dbed8547bbfc8b9810392c6c364592e883
bcmn <bcmn96@gmail.com>
2017-08-16 21:36:01 -0300

    Synchronise sidebar widths across tabs

--- WWDC/SessionsSplitViewController.swift
+++ WWDC/SessionsSplitViewController.swift
@@ -17,12 +17,14 @@ final class SessionsSplitViewController: NSSplitViewController {
     
     var listViewController: SessionsTableViewController
     var detailViewController: SessionDetailsViewController
+    var isResizingSplitView: Bool = false
     
     init(listStyle: SessionsListStyle) {
         listViewController = SessionsTableViewController(style: listStyle)
         detailViewController = SessionDetailsViewController(listStyle: listStyle)
         
         super.init(nibName: nil, bundle: nil)!
+        NotificationCenter.default.addObserver(self, selector: #selector(syncSplitView(notification:)), name: Notification.Name.NSSplitViewDidResizeSubviews, object: nil)
     }
     
     required init?(coder: NSCoder) {
@@ -43,6 +45,31 @@ final class SessionsSplitViewController: NSSplitViewController {
         listViewController.view.setContentHuggingPriority(NSLayoutPriorityDefaultHigh, for: .horizontal)
         detailViewController.view.setContentHuggingPriority(NSLayoutPriorityDefaultLow, for: .horizontal)
         detailViewController.view.setContentCompressionResistancePriority(NSLayoutPriorityDefaultHigh, for: .horizontal)
-    }    
+    }
+    
+    @objc private func syncSplitView(notification: Notification) {
+        guard isResizingSplitView == false else {
+            return
+        }
+        guard notification.userInfo?["NSSplitViewDividerIndex"] != nil else {
+            return
+        }
+        guard let otherSplitView = notification.object as? NSSplitView else {
+            return
+        }
+        guard otherSplitView != self.splitView else {
+            return
+        }
+        guard  splitView.subviews.count > 0, otherSplitView.subviews.count > 0 else {
+            return
+        }
+        guard splitView.subviews[0].bounds.width != otherSplitView.subviews[0].bounds.width else {
+            return
+        }
+        
+        isResizingSplitView = true
+        splitView.setPosition(otherSplitView.subviews[0].bounds.width, ofDividerAt: 0)
+        isResizingSplitView = false
+    }
 }
 
