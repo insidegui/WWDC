726842db5681288a40c11a481ffbf34f6f87060d
Allen Humphreys <allen.humphreys@icloud.com>
2018-03-19 11:37:02 -0500

    Two small fixes  * Changes cleardata.sh to include debug and no long need sudo  * Ensure window gets re-shown if app is activated via app switcher

--- cleardata.sh
+++ cleardata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rm -Rfv ~/Library/Application\ Support/io.wwdc.app/*
-rm ~/Library/Preferences/io.wwdc.app.plist
-sudo killall cfprefsd
+rm -Rfv ~/Library/Application\ Support/io.wwdc.app*
+defaults delete io.wwdc.app
