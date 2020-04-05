fa25178d1c55a5b0a5e7be2ead0cf0990b0680d5
Guilherme Rambo <insidegui@gmail.com>
2020-04-05 13:22:46 -0300

    Fixed content not loading when using new endpoint

--- WWDC/AppCoordinator.swift
+++ WWDC/AppCoordinator.swift
@@ -257,15 +257,15 @@ final class AppCoordinator {
         // Initial app launch waits for all of these things to be loaded before dismissing the primary loading spinner
         // It may, however, delay the presentation of content on tabs that already have everything they need
 
-        let starupDependencies = Observable.combineLatest(storage.tracksObservable,
+        let startupDependencies = Observable.combineLatest(storage.tracksObservable,
                                                           storage.eventsObservable,
                                                           storage.focusesObservable,
                                                           storage.scheduleObservable,
                                                           storage.featuredSectionsObservable)
 
-        starupDependencies
+        startupDependencies
             .filter {
-                !$0.0.isEmpty && !$0.1.isEmpty && !$0.2.isEmpty && !$0.3.isEmpty && !$0.4.isEmpty
+                !$0.0.isEmpty && !$0.1.isEmpty && !$0.2.isEmpty && !$0.4.isEmpty
             }
             .take(1)
             .subscribe(onNext: { [weak self] tracks, _, _, sections, _ in
