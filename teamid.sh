957a832a63426f852a7d8439a9b72629b158187d
Guilherme Rambo <insidegui@gmail.com>
2023-05-29 13:19:36 -0300

    Allowing teamid script to receive ID as its first argument

--- teamid.sh
+++ teamid.sh
@@ -2,13 +2,19 @@
 
 TEAM_ID_FILE=WWDC/TeamID.xcconfig
 
-echo "What is your Apple Developer Team ID? (looks like 1A23BDCD)"
-read TEAM_ID
+if [ -z "$1" ]; then
+  echo "What is your Apple Developer Team ID? (looks like 1A23BDCD)"
+  read TEAM_ID
 
-if [ -z "$TEAM_ID" ]; then
-  echo "You must enter a team id"
-  exit 1
+  if [ -z "$TEAM_ID" ]; then
+    echo "You must enter a team id"
+    exit 1
+  fi
+else
+  TEAM_ID=$1
 fi
+
+echo "Setting team ID to $TEAM_ID"
 
 echo "// This file was automatically generated, do not edit directly." > $TEAM_ID_FILE
 echo "" >> $TEAM_ID_FILE
