d184234edb867a1baf80342f53f6d20a5b7af653
Allen Humphreys <allen.humphreys@icloud.com>
2025-09-09 15:05:05 -0400

    Fix NSSearchField clear button behavior

--- WWDC/Util/SearchField.swift
+++ WWDC/Util/SearchField.swift
@@ -45,11 +45,23 @@ struct SearchField: NSViewRepresentable {
             self.parent = parent
         }
 
+        /// This is delayed via NSSearchField behavior
         @objc
-        func searchFieldAction(_ sender: Any) {
-            parent.onSearch()
+        func searchFieldAction(_ sender: NSSearchField) {
+            DispatchQueue.main.async { [parent] in
+                // controlTextDidChange guarantees immediate text entry updates are relayed to SwiftUI state,
+                // but when the clear button is clicked, the action is sent before that state update happens.
+                //
+                // Additionally, this is just prudent to ensure state is in sync before sending the search action.
+                if parent.text != sender.stringValue {
+                    parent.text = sender.stringValue
+                }
+
+                parent.onSearch()
+            }
         }
 
+        /// This is immediately called as the user types
         func controlTextDidChange(_ obj: Notification) {
             let value = (obj.object as? NSSearchField)?.stringValue ?? ""
             if parent.text != value {
